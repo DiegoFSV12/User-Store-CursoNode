ddd19a0b27316923f286e9ce587099c2f16ade2a
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-04 15:07:58 -0500

    Auth Service

--- src/presentation/service/auth.service.ts
+++ src/presentation/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { UserModel } from "../../data";
-import { CustomError, RegisterUserDTO } from "../../domain";
+import { CustomError, RegisterUserDTO, UserEntity } from "../../domain";
 
 export class AuthService{
     constructor(){}
@@ -7,6 +7,21 @@ export class AuthService{
     public async registerUser(registerDTO:RegisterUserDTO){
         const existUser = await UserModel.findOne({email:registerDTO.email});
         if(existUser) throw CustomError.badRequest('Email already exist');
-        return 'All OK';
+        try {
+            const user = new UserModel(registerDTO);
+            await user.save();
+            //Encriptar la contraseña
+
+            //JWT para mantener la autenitcación de usuario
+
+            //Email de confirmación
+
+            const {password, ...userEntity} = UserEntity.fromObject(user);
+            return {
+                user: userEntity,
+                token:'ABC'};
+        } catch (error) {
+            throw CustomError.internalServer(`${error}`);
+        }
     }
 }
