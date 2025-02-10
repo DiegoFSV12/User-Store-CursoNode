4abaefb8094433e3161c7f3b79a66e908b415ed2
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-10 15:25:59 -0500

    Rutas actualizadas

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -16,7 +16,8 @@ static get routes(): Router {
     const emailService = new EmailService(
         envs.MAILER_SERVICE,
         envs.MAILER_EMAIL,
-        envs.MAILER_SECRET_KEY
+        envs.MAILER_SECRET_KEY,
+        envs.SEND_EMAIL
     );
     const autheService = new AuthService(emailService);
     const controller = new AuthController(autheService);
