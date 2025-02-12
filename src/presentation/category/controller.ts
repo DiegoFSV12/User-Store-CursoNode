ee5812b9c7b1249dd0579e834a36b441c4ae9846
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-12 16:37:37 -0500

    Categories Controller

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express"
-import { CreateCategoryDTO, CustomError } from "../../domain";
+import { CreateCategoryDTO, CustomError, PaginationDTO } from "../../domain";
+import { CategoryService } from "../service/category.service";
 
 export class CategoryController{
-    constructor(){}
+    constructor(
+        private readonly categoryService:CategoryService,
+    ){}
 
     private handleError = (error:unknown,res:Response)=>{
         if(error instanceof CustomError){
@@ -13,13 +16,19 @@ export class CategoryController{
     }
 
     createCategory = async(req:Request, res:Response) =>{
-        // const [error,createCategoryDto] = CreateCategoryDTO.create(req.body);
-        // if(error) res.status(400).json(error);
-        // res.json(createCategoryDto);
-        res.json(req.body)
+        const [error,createCategoryDto] = CreateCategoryDTO.create(req.body);
+        if(error) res.status(400).json(error);
+        this.categoryService.createCategory(createCategoryDto!,req.body.user)
+            .then(category => res.status(201).json(category))
+            .catch(error => this.handleError(error,res));
     }
 
     getCategories = async(req:Request, res:Response) =>{
-        res.json('Get categories');
+        const{page=1,limit=10}=req.query;
+        const [error, paginationDto] = PaginationDTO.create(Number(page),Number(limit));
+        if(error) return res.status(400).json(error);
+        this.categoryService.getCategories(paginationDto!)
+            .then(category => res.status(201).json(category))
+            .catch(error => this.handleError(error,res));
     }
 }
