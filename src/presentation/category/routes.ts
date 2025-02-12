a981898587d6d0bd85e7ef83354dadc4e78359a2
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-12 16:38:18 -0500

    Category Routes

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
 import { AuthMiddleware } from '../middlewares/auth.middleware';
+import { CategoryService } from '../service/category.service';
 
 
 export class CategoryRoutes {
@@ -9,11 +10,12 @@ export class CategoryRoutes {
 static get routes(): Router {
 
     const router = Router();
-    const controller = new CategoryController();
+    const service = new CategoryService();
+    const controller = new CategoryController(service);
 
     // Definir las rutas
     router.get('/', controller.getCategories);
-    router.post('/', [AuthMiddleware.validateJWT],controller.createCategory);//Con el AuthMiddleware verificamos que haiga una sesión iniciada mediante el token que se genera
+    router.post('/', [AuthMiddleware.validateJWT],controller.createCategory);//Con el AuthMiddleware verificamos que haiga una sesión iniciada mediante el token que se genera y se guarda al usuario en la request
 
 
 
