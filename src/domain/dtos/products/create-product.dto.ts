ad80bf4d9c55bc2296397bb603f4a25fe4163feb
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-14 17:09:57 -0500

    CreateProductDTO

---
+++ src/domain/dtos/products/create-product.dto.ts
@@ -0,0 +1,30 @@
+export class CreateProductDTO{
+    private constructor(
+        public readonly name: string,
+        public readonly available: boolean,
+        public readonly price: number,
+        public readonly description: string,
+        public readonly user: string,//id
+        public readonly category: string,//id
+    ){}
+
+    static create(object:{[key:string]:any}):[string?,CreateProductDTO?]{
+        const {
+            name,
+            available=false,
+            price,
+            description,
+            user,
+            category
+        } = object;
+        let availableBoolean = available;
+        if(!name) return ['Missign name'];
+        if(!price) return ['Missign price'];
+        if(!user) return ['Missign user'];
+        if(!category) return ['Missign category'];
+        if(typeof available != 'boolean'){
+            availableBoolean = (available === 'true');
+        }
+        return [undefined,new CreateProductDTO(name,availableBoolean,price,description,user,category)];
+    }
+}
