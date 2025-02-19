bc8a2f8555b077fcda5af84f5c6c25ede3efbfaf
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-19 15:19:00 -0500

    FileUploadService

--- src/presentation/service/file-upload.service.ts
+++ src/presentation/service/file-upload.service.ts
@@ -1,16 +1,29 @@
+import { UploadedFile } from "express-fileupload";
+import path from "path";
+import fs from 'fs';
+
 export class FileUploadService{
     constructor(){}
 
     private checkFolder(folderPath:string){
-        
+        if(!fs.existsSync(folderPath)){
+            fs.mkdirSync(folderPath);
+        }
     }
 
     async uploadFile(
-        file: any,
+        file: UploadedFile,
         folder: string = 'uploads',
         validExtensions: string[] = ['png','jpg','jpeg','gif']
     ){
-
+        try {
+            const fileExtension = file.mimetype.split('/').at(1);//Corta el mimetype para obtener el tipo de archivo (png,jpg,jpeg...)
+            const destination = path.resolve(__dirname,'../../../',folder);
+            this.checkFolder(destination);
+            file.mv(destination+`/my-image.${fileExtension}`);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async uploadMultipleFile(
