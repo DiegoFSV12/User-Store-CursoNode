f657fc54aafc09eda94032cb0d7ae5022e9f0c1d
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-18 20:40:54 -0500

    FileUploadService

---
+++ src/presentation/service/file-upload.service.ts
@@ -0,0 +1,25 @@
+export class FileUploadService{
+    constructor(){}
+
+    private checkFolder(folderPath:string){
+        
+    }
+
+    async uploadFile(
+        file: any,
+        folder: string = 'uploads',
+        validExtensions: string[] = ['png','jpg','jpeg','gif']
+    ){
+
+    }
+
+    async uploadMultipleFile(
+        file: any[],
+        folder: string = 'uploads',
+        validExtensions: string[] = ['png','jpg','jpeg','gif']
+    ){
+        
+    }
+
+    
+}
