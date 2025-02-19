cb358f2ee4f3e4900bf65b3c052a9dd364d7e31b
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-19 15:19:08 -0500

    Service Controller

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express"
 import { CustomError } from "../../domain";
+import { FileUploadService } from "../service/file-upload.service";
+import { UploadedFile } from "express-fileupload";
 
 
 export class FileUploadController{
     constructor(
-        
+        private readonly fileUploadService:FileUploadService,
     ){}
 
     private handleError = (error:unknown,res:Response)=>{
@@ -16,8 +18,13 @@ export class FileUploadController{
     }
 
     uploadFile = async(req:Request, res:Response) =>{
-        console.log({file: req.files});
-        res.json('Upload file');
+        if(!req.files || Object.keys(req.files).length === 0){
+            return res.status(400).json({error: 'No files were selected'});
+        }
+        const file = req.files.file as UploadedFile;
+        this.fileUploadService.uploadFile(file)
+            .then(file => res.json(file))
+            .catch(error => this.handleError(error,res));
     }
 
     uploadMultipleFile = async(req:Request, res:Response) =>{
