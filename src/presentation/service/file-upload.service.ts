d90e2772ff86a6a117ecf7aa37051fd332458afc
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-19 20:49:53 -0500

    FileUploadService

--- src/presentation/service/file-upload.service.ts
+++ src/presentation/service/file-upload.service.ts
@@ -1,9 +1,13 @@
 import { UploadedFile } from "express-fileupload";
 import path from "path";
 import fs from 'fs';
+import { Uuid } from "../../config";
+import { CustomError } from "../../domain";
 
 export class FileUploadService{
-    constructor(){}
+    constructor(
+        private readonly uuid = Uuid.v4,
+    ){}
 
     private checkFolder(folderPath:string){
         if(!fs.existsSync(folderPath)){
@@ -17,12 +21,17 @@ export class FileUploadService{
         validExtensions: string[] = ['png','jpg','jpeg','gif']
     ){
         try {
-            const fileExtension = file.mimetype.split('/').at(1);//Corta el mimetype para obtener el tipo de archivo (png,jpg,jpeg...)
+            const fileExtension = file.mimetype.split('/').at(1) ?? '';//Corta el mimetype para obtener el tipo de archivo (png,jpg,jpeg...)
+            if(!validExtensions.includes(fileExtension)){
+                throw CustomError.badRequest(`Invalid extension : ${fileExtension}, valid ones ${validExtensions}`)
+            }
             const destination = path.resolve(__dirname,'../../../',folder);
             this.checkFolder(destination);
-            file.mv(destination+`/my-image.${fileExtension}`);
+            const fileName = `${this.uuid()}.${fileExtension}`;
+            file.mv(`${destination}/${fileName}`);
         } catch (error) {
             console.log(error);
+            throw error;
         }
     }
 
