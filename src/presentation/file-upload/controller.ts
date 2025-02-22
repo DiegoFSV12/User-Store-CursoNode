b49ad8c39e719e984c3b7456d298af07da9a9c9e
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-02-22 16:05:00 -0500

    FileUploadController

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import { CustomError } from "../../domain";
 import { FileUploadService } from "../service/file-upload.service";
 import { UploadedFile } from "express-fileupload";
+import { isArray } from "util";
 
 
 export class FileUploadController{
@@ -18,12 +19,9 @@ export class FileUploadController{
     }
 
     uploadFile = async(req:Request, res:Response) =>{
-        const type = req.params.type;
-        const validTypes = ['users','products','categories'];
-        if(!validTypes.includes(type)){
-            return res.status(400).json({error: `Invalid type: ${type}, valid ones ${validTypes}`});
-        }
-        
+        const type = req.url.split('/').at(2);//el req.params.type ya no funciona xq el middleware que valida el type se ejecuta antes
+        const num = req.files!.file as UploadedFile[];
+        if(num.length != undefined) return res.status(400).json('No more than oner file on this route');
         const file = req.body.files.at(0) as UploadedFile;
         this.fileUploadService.uploadFile(file,`uploads/${type}`)
             .then(file => res.json(file))
@@ -31,6 +29,12 @@ export class FileUploadController{
     }
 
     uploadMultipleFile = async(req:Request, res:Response) =>{
-        res.json('Upload multiple file');
+        const type = req.url.split('/').at(2);
+        const files = req.files!.file as UploadedFile[];
+        this.fileUploadService.uploadMultipleFile(files,`uploads/${type}`)
+            .then(file => res.json(file))
+            .catch(error => this.handleError(error,res));
     }
+
+    
 }
